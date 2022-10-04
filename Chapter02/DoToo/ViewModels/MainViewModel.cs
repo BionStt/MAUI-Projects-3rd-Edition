a2eaf89fb6b726102e09bfd1f7609cbeb797a145
Michael Cummings <cummings.michael@live.com>
2022-10-04 11:37:30 -0400

    Navigating from MainView to ItemView

--- Chapter02/DoToo/ViewModels/MainViewModel.cs
+++ Chapter02/DoToo/ViewModels/MainViewModel.cs
@@ -1,19 +1,25 @@
 ﻿namespace DoToo.ViewModels;
 
+using CommunityToolkit.Mvvm.Input;
 using DoToo.Repositories;
+using DoToo.Views;
 
-public class MainViewModel : ViewModel
+public partial class MainViewModel : ViewModel
 {
     private readonly ITodoItemRepository repository;
+    private readonly IServiceProvider services;
 
-    public MainViewModel(ITodoItemRepository repository)
+    public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
     {
-        this.repository = repository; 
+        this.repository = repository;
+        this.services = services;
         Task.Run(async () => await LoadData());
     }
-
     private async Task LoadData()
     {
-
     }
+
+    [RelayCommand]
+    public async Task AddItem() => await Navigation.PushAsync(services.GetRequiredService<ItemView>());
+
 }
